846aaeb16f07daabfe72b61553488c04ffbd089a
Unknown <kennethjiepadasas@gmail.com>
2018-03-29 01:07:31 +0800

    Auto stash before merge of "master" and "origin/master"

--- admin-manage-students.php
+++ admin-manage-students.php
@@ -72,6 +72,29 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-
+ <footer class="footer9">
+        <div class="row" id="row1">
+            <div class="col-md-3 col-md-offset-0 col-sm-6 footer-navigation" id="col1">
+                <h3 id="foot-head1"> St. Anthony's College</h3>
+                <p class="company-name">Copyright © 2017 </p>
+            </div>
+            <div class="col-md-3 footer-contacts">
+                <div id="sec1"><i class="fa fa-phone footer-contacts-icon"></i>
+                    <p id="par2" class="footer-center-info email"> (036) 540-9238</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 footer-contacts">
+                <div id="sec3"><i class="fa fa-fax footer-contacts-icon"></i>
+                    <p id="par3">(036) 540-9971; 540-9196 </p>
+                </div>
+            </div>
+            <div class="col-md-3 footer-contacts">
+                <div id="sec2"><span class="fa fa-map-marker footer-contacts-icon"> </span>
+                    <p id="par1"><span class="new-line-span">San Jose de Buenavista</span> Antique, Philippines</p>
+                </div>
+            </div>
+            <div class="clearfix visible-sm-block"></div>
+        </div>
+    </footer>
 </body>
 </html>
--- admin-manage-voting-results.php
+++ admin-manage-voting-results.php
@@ -62,6 +62,30 @@
     </div>
 
   </div>
+   <footer class="footer9">
+        <div class="row" id="row1">
+            <div class="col-md-3 col-md-offset-0 col-sm-6 footer-navigation" id="col1">
+                <h3 id="foot-head1"> St. Anthony's College</h3>
+                <p class="company-name">Copyright © 2017 </p>
+            </div>
+            <div class="col-md-3 footer-contacts">
+                <div id="sec1"><i class="fa fa-phone footer-contacts-icon"></i>
+                    <p id="par2" class="footer-center-info email"> (036) 540-9238</p>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 footer-contacts">
+                <div id="sec3"><i class="fa fa-fax footer-contacts-icon"></i>
+                    <p id="par3">(036) 540-9971; 540-9196 </p>
+                </div>
+            </div>
+            <div class="col-md-3 footer-contacts">
+                <div id="sec2"><span class="fa fa-map-marker footer-contacts-icon"> </span>
+                    <p id="par1"><span class="new-line-span">San Jose de Buenavista</span> Antique, Philippines</p>
+                </div>
+            </div>
+            <div class="clearfix visible-sm-block"></div>
+        </div>
+    </footer>
 
 </body>
 </html>
